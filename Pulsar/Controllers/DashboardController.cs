10f314b47cfc63bc060d7ba74e62e93c8fe3d0ab
Wouter Bos <wouter@bososs.nl>
2015-10-24 23:12:54 +0200

    Initial commit

---
+++ Pulsar/Controllers/DashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Pulsar.Models;
+using Telerik.TeamPulse.Sdk;
+using Telerik.TeamPulse.Sdk.Models;
+
+namespace Pulsar.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var settings = new TeamPulseAppSettings()
+            {
+                SiteUrl = "XXX",
+                UseWindowsAuth = true,
+                Username = "XXX",
+                Password = "XXX",
+                Domain = "XXX"
+            };
+
+            TeamPulseApp app = new TeamPulseApp(settings);
+            app.Login();
+            WorkItem[] workitems = app.WorkItems.Get().results;
+
+            var model = new DashboardPageModel { };
+            return View(model);
+        }
+    }
+}
---
+++ Pulsar/Models/DashboardModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pulsar.Models
+{
+    public class DashboardPageModel
+    {
+        public string ApplicationName
+        {
+            get
+            {
+                return System.Configuration.ConfigurationManager.AppSettings["ApplicationName"];
+            }
+        }
+    }
+}
