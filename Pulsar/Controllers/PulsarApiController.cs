b9a71df481fe23d5fa94fdd3a3c1a4c2dbcd8aad
Wouter Bos <wouter@bososs.nl>
2015-10-30 01:08:24 +0100

    Getting account and iteration info

--- Pulsar/Controllers/PulsarApiController.cs
+++ Pulsar/Controllers/PulsarApiController.cs
@@ -6,13 +6,14 @@ using System.Net.Http;
 using System.Web.Http;
 using Telerik.TeamPulse.Sdk;
 using Telerik.TeamPulse.Sdk.Models;
+using Newtonsoft.Json;
 
 namespace Pulsar.Controllers
 {
     public class PulsarApiController : ApiController
     {
         [HttpGet]
-        public virtual int Login(string Username, string Password, string Domain)
+        public virtual string Login(string Username, string Password, string Domain)
         {
             var settings = new TeamPulseAppSettings()
             {
@@ -25,9 +26,23 @@ namespace Pulsar.Controllers
 
             TeamPulseApp app = new TeamPulseApp(settings);
             app.Login();
-            WorkItem[] workitems = app.WorkItems.Get().results;
+            DateTime date = DateTime.Now;
+            Iteration iteration = app.Iterations.GetAll("$filter=startDate lt DateTime'" + date.ToString("yyyy-MM-dd") + "' and endDate gt DateTime'" + date.ToString("yyyy-MM-dd") + "'&$orderby=startDate desc").results.FirstOrDefault();
+            User user = app.Users.GetCurrent();
 
-            return app.WorkItems.Get().results.Count();
+            var result = new {
+                iteration = new {
+                    id = iteration.id,
+                    startDate = iteration.startDate.ToString("yyyy-MM-dd"),
+                    endDate = iteration.endDate.ToString("yyyy-MM-dd")
+                },
+                accountInfo = new
+                {
+                    displayName = user.displayName
+                }
+            };
+
+            return JsonConvert.SerializeObject(result);
         }
     }
 }
--- Pulsar/Scripts/Pulsar.ts
+++ Pulsar/Scripts/Pulsar.ts
@@ -2,20 +2,50 @@
 /// <reference path="knockout.d.ts" />
 
 module pulsar {
-    export module api {
-        export function login(username: string, password: string, domain: string): void {
+    export class api {
+        private accountInfo: IAccountInfo;
+        private iteration: IIteration;
+
+        constructor(username: string, password: string, domain: string) {
+            this.accountInfo = {
+                username: username,
+                password: password,
+                domain: domain,
+                displayName: ''
+            }
+        }
+
+        login(): void {
             jQuery.ajax({
-                url: '/api/PulsarApi/Login?Username=' + username + '&Password=' + password + '&Domain=' + domain,
+                url: '/api/PulsarApi/Login?Username=' + this.accountInfo.username + '&Password=' + this.accountInfo.password + '&Domain=' + this.accountInfo.domain,
                 success: d => {
-                    console.log(d);
+                    this.iteration = JSON.parse(d).iteration;
+                    this.accountInfo.displayName = JSON.parse(d).accountInfo.displayName;
+                    console.log(this.iteration, this.accountInfo);
+
                 }
             });
         }
     }
 
     export module page {
+        var api: pulsar.api;
         export function init(): void {
-            pulsar.api.login('xxx', 'xxx', 'xxx');
+            api = new pulsar.api('wouter.bos', 'xxx', 'xxx');
+            api.login();
         }
+    }
+
+    export interface IAccountInfo {
+        username: string
+        password: string
+        domain: string
+        displayName: string
+    }
+
+    export interface IIteration {
+        id: number
+        startDate: string
+        endDate: string
     }
 }
